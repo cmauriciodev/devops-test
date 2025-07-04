fa92ba46495793d0a0c244e8f367df05cb9f83c0
Carlos Mauricio <cmdeveloper97@gmail.com>
2025-07-04 14:16:56 -0600

    redploy backend

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT || 3001;
 const cors = require('cors'); 
  
 app.use(cors());  
-  
+   
 app.get("/api", (req, res) => {    
-  res.json({ message: "Hola desde Express!" });
+  res.json({ message: "Hola desde Express!" }); 
 });
 
 app.listen(PORT, '0.0.0.0', () => {
