de2148d51ce7cb5bd52def4bdbf59c90e125bf85
Carlos Mauricio <cmdeveloper97@gmail.com>
2025-07-03 22:58:56 -0600

    feat: crdeate project

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const cors = require('cors');
+
+app.use(cors());
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hola desde Express!" });
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("Cargando...");
+
+  useEffect(() => {
+    fetch(import.meta.env.VITE_API_URL)
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+      .catch(() => setMessage("Error al conectar con backend"));
+  }, []);
+
+  return (
+    <div style={{ padding: "2rem", fontSize: "1.5rem" }}>
+      <p>Hola desde React</p>
+      <p>Backend dice: {message}</p>
+    </div>
+  );
+}
+
+export default App;
