0e79a4a63b148eb85f370320877c16e07642c4e4
Carlos Mauricio <cmdeveloper97@gmail.com>
2025-07-04 14:10:47 -0600

    redploy backend

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const cors = require('cors');
+const cors = require('cors'); 
  
 app.use(cors()); 
   
