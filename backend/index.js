f7bd5f41b36e228a4944787b3ec714d5255585a4
Carlos Mauricio <cmdeveloper97@gmail.com>
2025-07-04 16:42:46 -0600

    redepploy backend

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 app.use(cors());  
     
 app.get("/api", (req, res) => {     
-  res.json({ message: "Hola desde Express!" });  
+  res.json({ message: "Hola desde Express v2!" });  
 });
   
 app.listen(PORT, '0.0.0.0', () => {
