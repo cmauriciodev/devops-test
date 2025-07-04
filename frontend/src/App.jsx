28210091e2a34b7d4fb4ffcf07a298a8a810101e
Carlos Mauricio <cmdeveloper97@gmail.com>
2025-07-03 23:57:56 -0600

    fix: frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return ( 
     <div style={{ padding: "2rem", fontSize: "1.5rem" }}>
       <p>Hola desde React</p> 
-      <p>Backend dice: {message}</p>
+      <p>Backend dice: {message}</p> 
     </div> 
   ); 
 }
